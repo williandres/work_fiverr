cb75ca5298afa9ab94fe24c0e5efae3d107f1446
Willian <sca.willian@gmail.com>
2022-05-27 19:07:02 -0500

    Automatico

--- functions.py
+++ functions.py
@@ -1,10 +1,14 @@
-def upload_file(file):
+def read_file(num):
     data = []
     try:
-        with open(f"./data/{file}","r", encoding = "utf-8") as f:
+        with open(f"./data/test_0{num}_p60.csv","r", encoding = "utf-8") as f:
             tittles = f.readline().split(",")
             for line in f:
                 data.append(line.split(","))
-        return data
+        print(data)
     except FileNotFoundError:
-        print("Archivo no ha sido encontrado")
+        pass
+
+def fileCsv():
+    for i in range (265465, 265480):
+        read_file(i)
--- main.py
+++ main.py
@@ -1,30 +1,6 @@
-import os
 import functions as func
-def ui():
-    os.system('cls')
-    print("""
-    ¿Que quieres hacer?
-
-    1. Cargar un archivo csv
-    2. Consultar
-    3. Salir
-    """)
 
 def run():
-    ui()
-    execution = True
-    while execution:
-        try:
-            option = int(input("Escoge una opcion segun su numero: "))
-            if option == 1:
-                file = input("Digite el nombre su archivo(.csv) completo: ")
-                print(func.upload_file(file))
-            if option == 2:
-                pass
-            if option == 3:
-                execution = False
-        except ValueError:
-            print("Escoga una opcion correcta")
-    print("Gracias!!!")
+    func.fileCsv()
 if __name__== "__main__":
     run()
