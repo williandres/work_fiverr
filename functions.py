faa9cb164f1e4a1a2c406bc2352a5079fbc0c595
Willian <sca.willian@gmail.com>
2022-05-31 13:41:25 -0500

    Prueba

--- functions.py
+++ functions.py
@@ -1,14 +1,44 @@
+import csv
+
 def read_file(num):
-    data = []
+    data = [["IdVehiculo","KilometrosOdometro"]]
     try:
-        with open(f"./data/test_0{num}_p60.csv","r", encoding = "utf-8") as f:
-            tittles = f.readline().split(",")
-            for line in f:
-                data.append(line.split(","))
-        print(data)
+        with open(f"./test/{num}_p60.csv","w", encoding = "utf-8") as f:
+                writer = csv.writer(f)
+                writer.writerows(data)
     except FileNotFoundError:
         pass
 
 def fileCsv():
-    for i in range (265465, 265480):
+    for i in range (20220404, 20220431):
         read_file(i)
+
+def ord(data):
+    pass
+
+"""
+ALGORITMOS 
+Se puede usar el merge sort pero necesita su tiempo para entenderlo
+Bubble sort
+    n = len(lista)
+
+    for i in range(n):
+        for j in range(0, n - i - 1): # O(n) * O(n) = O(n * n) = O(n**2)
+
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+Binary Search
+    print(f'buscando {objetivo} entre {lista[comienzo]} y {lista[final - 1]}')
+    if comienzo > final:
+        return False
+
+    medio = (comienzo + final) // 2 #division entera
+
+    if lista[medio] == objetivo:
+        return True
+    elif lista[medio] < objetivo:
+        return busqueda_binaria(lista, medio + 1, final, objetivo)
+    else:
+        return busqueda_binaria(lista, comienzo, medio - 1, objetivo)
+
+"""
