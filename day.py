46ce74a03c3230f9c325313e3afbd5e6a8e1f7c9
Willian <sca.willian@gmail.com>
2022-06-06 16:48:17 -0500

    Final Version v1.4

--- day.py
+++ day.py
@@ -1,7 +1,15 @@
 import csv
-# from datetime import datetime
 
 def read_buses(desde,hasta):
+    """Funcion que convierte todos las filas de 'telemetria_funcionamiento.csv' en una lista con los id_buses. 
+       -buses = list : crea una lista a con todos los id_buses
+       
+       Argumentos que posteriormente se envian a la funcion buses_day
+       -tittles_buses = list, captura los titulos para posteriormente añadir nuevas columnas.
+       -desde = int, argumento que determina el incio del rango de los archivos por explorar. 
+       -hasta = int, argumento que determina el final del rango de los archivos por explorar.
+    
+    """
     with open(f"./telemetria_funcionamiento.csv","r", encoding = "utf-8") as f:
         reader = csv.reader(f)
         tittles_buses = next(reader)
@@ -12,6 +20,16 @@ def read_buses(desde,hasta):
     buses_day(buses,tittles_buses,desde,hasta)
 
 def buses_day(buses,tittles_buses,desde,hasta):
+    """Funcion encargada de crear una lista 'data' con todas las filas dentro del archivo telemetria por dia(csv)
+       posteriormente llama una funcion buses_evaluate()  
+        Argumentos que posteriormente se enviaran a buses_evaluate() 
+       -tittles_buses = list, captura los titulos para posteriormente añadir nuevas columnas.
+       -desde = int, argumento que determina el incio del rango de los archivos por explorar. 
+       -hasta = int, argumento que determina el final del rango de los archivos por explorar.
+       -buses = list, argumento con la lista de todos los buses
+       -data = list, arguemento con la lista de todas las filas del archivo telemetria por dia(csv)
+       -i = int, es el indicador del titulo del archivo, que se puede usar como fecha 
+    """
     for i in range (desde,hasta+1):
         data = []
         try:
@@ -24,18 +42,24 @@ def buses_day(buses,tittles_buses,desde,hasta):
             pass
 
 def buses_evaluate(data,buses,num,tittles):
+    """Funcion encargada de insertar el titulo de segun el dia y si el id_bus ese dia esta : "Sin Registro","Sin datos","Dato irregular" o "Dato correcto";
+        -date = str : apartir del argumento 'num' lo convertimos en una fecha con buena estandarizacion.
+        -correcto =  int : variable que se actualiza en el caso que el id bus encuentre una linea en 'data' donde el kilometraje sea mayor a 0.
+        -total = int : variable que se actualiza en el caso que el id bus encuentre su id en una linea de 'data'.
+        Al terminar de construir la lista 'buses' llama a la funcion write_buses() donde escribe la lista en el csv
+    """
     date = str(num)[6:]+"-"+str(num)[4:6]+"-"+str(num)[:4]
     tittles.append(date)
     print(f'Construyendo telemetria de la fecha: {date}...')
     for bus in buses:
         correcto = 0
         total = 0
-        for line in data:
+        for line in data: #Bucle donde evalua linea por linea
             if bus[0] == line[3]:
                 total += 1
             if bus[0] == line[3] and float(line[9]) > 0:
                 correcto += 1
-        if correcto == 0 and total == 0:
+        if correcto == 0 and total == 0: #Condicionales que segun 'correcto' y 'total' insertan el tipo de dato que es
             bus.append("Sin Registro")
         elif correcto == 0 and total > 0:
             bus.append("Sin datos")
@@ -46,6 +70,8 @@ def buses_evaluate(data,buses,num,tittles):
     write_buses(buses,tittles)
 
 def write_buses(buses,tittles):
+    """Funcion encargada de escribir los titulos y los buses con la lista ya actualizada en el archivo "telemetria_funcionamiento.csv"
+    """
     with open(f"./telemetria_funcionamiento.csv","w", encoding = "utf-8", newline='') as f:
         writer = csv.writer(f)
         writer.writerow(tittles)
--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-import buses as bus
-import day
-import time as ti
+import buses as bus # buses.py
+import day #  day.py
+import time as ti # Libreria para calcular el tiempo
 
 def run():
+    """ Funcion de ejecucion
+        -inicio = ti.time(),final = ti.time():Las usamos para capturar el tiempo preciso en sus determinados
+         momentos, para luego usarlos mostrar el tiempo de ejecucion del codigo.
+        
+        -bus.fileCsv(20220404,20220428) : llamamos la funcion fileCsv que se encuentra en el archivo buses.py.
+         Dicho archivo es el encargado de construir un csv con los id_buses que se encuentra en un cierto rango en los csv de telemetria;
+         rango el cual se determina desde arg1 hasta arg2 '20220404,20220428'.
+        
+        -day.read_buses(20220404,20220428) : llamamos la funcion read_buses que se encuentra en el archivo day.py.
+         Dicho archivo esta encargado de construir una columna por dia en el que por id_buses determine si en ese dia  fue registrado,
+         tiene datos correctos, datos irregulares o sin datos. Establece una columna de cada dia por un determinado rango;
+         rango el cual se determina desde arg1 hasta arg2 '20220404,20220428.
+    """
     inicio = ti.time()
     bus.fileCsv(20220404,20220428)
     print(f'Todos los buses han sido registrados')
     day.read_buses(20220404,20220428)
     final = ti.time()
     print(f'Telemetria por dia construida, Tiempo de ejecucion: {round(final-inicio)} segundos')
+
 if __name__== "__main__":
     run()
