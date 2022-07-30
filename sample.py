d9f7f566bb014164fbe4c839f784bdf8ab117181
Willian Cárdenas <sca.willian@gmail.com>
2022-07-30 09:58:36 -0500

    first try

--- sample.py
+++ sample.py
@@ -1,8 +1,33 @@
 import subprocess
+import os
+import time
+import time
+from pynput.keyboard import Key, Controller
 
+def install(exe):
+    keyboard = Controller()
+    os.startfile(exe)
+
+    time.sleep(1)
+
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+
+    time.sleep(5)
+
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
 
 def run():
-    subprocess.popen(r"Magic Icons (ask4pc) 10S.exe")
+    dir = [ f.path for f in os.scandir('./assets') if f.is_dir() ]
+    for dirs in dir:
+        for f in os.listdir(dirs):
+            if f[-4:] == '.exe':
+                path = f
+        install(dirs + "/" + path)
 
 
 if __name__ == '__main__':
