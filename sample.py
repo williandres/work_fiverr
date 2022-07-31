90bfa5430259c4742b9c70c0e1253c381f0442f8
Willian Cárdenas <sca.willian@gmail.com>
2022-07-30 20:21:58 -0500

    final 1

--- sample.py
+++ sample.py
@@ -1,11 +1,10 @@
-import subprocess
 import os
 import time
 from pynput.keyboard import Key, Controller
 
 def install(exe):
     keyboard = Controller()
-    os.system(exe)
+    os.startfile(exe)
 
     time.sleep(1)
 
@@ -21,11 +20,12 @@ def install(exe):
     keyboard.release(Key.enter)
 
 def run():
+    path = os.getcwd()
     dir = [ f.path for f in os.scandir('./') if f.is_dir() ]
     for dirs in dir:
         for f in os.listdir(dirs):
             if f[-4:] == '.exe':
-                install(dirs + "/" + f)
+                install(f'{path}\{dirs[2:]}\{f}')
 
 if __name__ == '__main__':
     run()
