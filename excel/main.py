57b1764fd26bebd8b90fc036187bab19b40095c6
Willian Cárdenas <sca.willian@gmail.com>
2022-12-07 18:18:41 -0500

    Default csv

--- excel/main.py
+++ excel/main.py
@@ -1,8 +1,78 @@
 import csv
 import os
 
+def default(l):
+    row = [0 for x in l]
+    row[0] = l[0]
+    return row
+
+def arrd(l):
+    l[1] = "Arrendador"
+
+    return l
+
+def arrt(l):
+    l[1] = "Arrendatario"
+
+    return l
+def fia(l):
+    l[1] = "Fiador"
+
+    return l
+def ase(l):
+    l[1] = "Asesor inmobiliario"
+
+    return l
+
 def new_csv(f):
-    print(f)
+    file = [[
+            'CONTRATO (FOLIO)',
+            'PERSONALIDAD',
+            '¿RENOVACION?',
+            'NOMBRE',
+            'TOTAL',
+            'IMPUESTO',
+            'DOMICILIO',
+            'NUMERO CONTACTO 1',
+            'NUMERO CONTACTO 2',
+            'CORREO ELECTRONICO',
+            'DOMICILIO INMUEBLE',
+            'MONTO RENTA',
+            'SE PAGA APARTIR DEL DIA',
+            'FECHA CONTRATO ARRENDAMIENTO',
+            'FECHA ARRENDAMIENTO INICIO ',
+            'VIGENCIA ARRENDAMIENTO',
+            'FECHA ARRENDAMIENTO TERMINO',
+            'VIGENCIA GJI',
+            'COBERTURA',
+            'MONTO OPERACIÓN',
+            'MONTO EN LETRA',
+            'COMISION',
+            'INMOBILIARIA',
+            'COMENTARIOS',
+            'VALOR PAGARE',
+            'KTOF',
+            ]]
+    for line in f:
+        #Arrendador
+        row = arrd(default(line))
+        file.append(row)
+        #Arrendatario
+        row = arrt(default(line))
+        file.append(row)
+        #Fiador
+        row = fia(default(line))
+        file.append(row)
+        #Asesor inmobiliario
+        row = ase(default(line))
+        file.append(row)
+
+    return file
+
+def download(new_csv):
+    with open('GJI - OCT NOV DIC - Nuevo.csv', 'w') as f:
+        write = csv.writer(f)
+        write.writerows(new_csv)
 
 def read_csv(file_name):
     with open(f"{file_name}","r", encoding = "utf-8") as f:
@@ -15,5 +85,6 @@ def read_csv(file_name):
 
 def main():
     old_csv = read_csv('GJI - OCT NOV DIC.xlsx - Original.csv')
-    new_csv(old_csv)
+    download(new_csv(old_csv))
+
 if __name__ == '__main__':main()
