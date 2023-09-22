c9b57d7e54f4db3ef2db95fc6c5c4d24f284ef6f
Willian Cárdenas <sca.willian@gmail.com>
2023-09-22 13:19:49 -0500

    auto positive thoughts

---
+++ auto_positive_affirmations/main.py
@@ -0,0 +1,25 @@
+import openai
+
+openai.api_key = ""
+with open("ideas.txt", mode="r", encoding="utf-8") as archivo_ideas:
+    ideas = archivo_ideas.read().splitlines()
+
+nuevas_ideas = []
+for _ in range(30):
+    prompt = "Create a unique and short positive thought"
+    respuesta = openai.Completion.create(
+        prompt=prompt,
+        max_tokens=100,
+        model="text-davinci-003",
+        temperature=0.9,
+        top_p=1,
+        frequency_penalty=1,
+        presence_penalty=1
+    )
+    print(respuesta.choices[0].text.strip())
+    nuevas_ideas.append(respuesta.choices[0].text.strip())
+
+ideas.extend(nuevas_ideas)
+with open("ideas.txt", mode="w", encoding="utf-8") as archivo_txt:
+    for idea in ideas:
+        archivo_txt.write(idea + "\n")
