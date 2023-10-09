f91ebeff6b2cf053dd7dc1c8be96dda1aa7d44fb
Willian Cárdenas <sca.willian@gmail.com>
2023-10-09 11:12:25 -0500

    finally

--- google_contacts/cut.py
+++ google_contacts/cut.py
@@ -2,9 +2,9 @@ import csv
 import os
 from datetime import datetime
 from dateutil import parser
-
+"""
 # Carpeta de origen que contiene los archivos CSV
-carpeta_origen = "sources/shiet/"
+carpeta_origen = "sources/contacts"
 
 # Carpeta de destino para los archivos CSV separados
 carpeta_destino = "sources/usa/"
@@ -16,21 +16,39 @@ print(archivos_de_origen)
 print(len(archivos_de_origen))
 
 """
+
+import os
 import pandas as pd
 
-# Establece el número máximo de filas por archivo
-filas_por_archivo = 10000
+# Ruta de la carpeta con los archivos CSV de entrada
+carpeta_contactos = 'sources/contacts_fixed'
 
-# Abre el archivo CSV original y crea un lector CSV
-with open('sources/contacts.csv', 'r') as archivo_original:
-    lector_csv = pd.read_csv(archivo_original, iterator=True, chunksize=filas_por_archivo)
+# Ruta de la carpeta para guardar los archivos divididos
+carpeta_divididos = 'sources/contacts_final'
+
+# Establece el número máximo de filas por archivo
+filas_por_archivo = 3000
+
+# Obtener la lista de archivos CSV en la carpeta de entrada
+archivos_csv = [f for f in os.listdir(carpeta_contactos) if f.endswith('.csv')]
+
+
+# Función para dividir un archivo CSV en archivos más pequeños
+def dividir_csv(archivo):
+    ruta_completa = os.path.join(carpeta_contactos, archivo)
+    lector_csv = pd.read_csv(ruta_completa, iterator=True, chunksize=filas_por_archivo)
     i = 1
 
     for chunk in lector_csv:
-        archivo_salida = f'sources/results/division_{i}.csv'
+        archivo_salida = os.path.join(carpeta_divididos, f'division_{i}_{archivo}')
         chunk.to_csv(archivo_salida, index=False)
         i += 1
 
-print("División completa.")
-"""
+# Procesar cada archivo CSV en la carpeta de entrada
+for archivo_csv in archivos_csv:
+    dividir_csv(archivo_csv)
+
+print("División completa. Los archivos divididos se encuentran en la carpeta 'divided'.")
+
+
 
