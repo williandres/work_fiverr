4356bbe0f18328d7062e100b88804e0b3ee2fbc1
Willian Cárdenas <sca.willian@gmail.com>
2023-10-19 06:32:45 -0500

    #

--- read_csv_error/try.py
+++ read_csv_error/try.py
@@ -1,14 +1,26 @@
 import pandas as pd
 
-file_csv = "Main_data.csv"
+file_csv = "sources/Main_data.csv"
 
-with open(file_csv, "r", encoding="latin-1") as file:
-    final_rows = []
-    for line in file:
-        row = line.strip().split("\t")
-        final_rows.append(row)
-df1 = pd.DataFrame(final_rows)
+cods = ["utf-8", "iso-8859-1", "windows-1252", "latin-1"]
 
+for cod in cods:
+    try:
+        with open(file_csv, "r", encoding=cod) as file:
+            final_rows = []
+            for line in file:
+                try:
+                    row = line.strip().split("\t")
+                    final_rows.append(row)
+                except Exception as e:
+                    print(f"Error on row: {line}")
+                    print(f"Error details: {e}")
+        df1 = pd.DataFrame(final_rows)
+        break
+    except UnicodeDecodeError:
+        continue
+else:
+    print("Impossible to read")
 
 print(df1.head(10))
 print(df1.tail(10))
