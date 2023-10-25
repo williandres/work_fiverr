dd861e16a32339dd32369d72226cb299f3a1802c
Willian Cárdenas <sca.willian@gmail.com>
2023-10-24 20:37:38 -0500

    without @

--- fast_clean/main.py
+++ fast_clean/main.py
@@ -9,7 +9,6 @@ xls = pd.ExcelFile(archivo_excel)
 # Leer la primera hoja "OGC"
 ogc_data = xls.parse("OGC", usecols="B", header=None, skiprows=1, nrows=45).iloc[:, 0].str.rstrip(',').tolist()
 
-
 # Leer la segunda hoja "esemble" y dividir la cadena por ";"
 esemble_df = pd.read_excel(xls, "ensemble", header=None, usecols="B", skiprows=1)
 esemble_data = esemble_df.iloc[0, 0].split(";")
@@ -24,7 +23,6 @@ cheer_data = cheer_df.iloc[0, 0].split(";")
 
 all_data = ogc_data + esemble_data + track_data + cheer_data
 
-#Csv Shit
 # Crear una lista para almacenar los datos procesados
 csv_data = []
 
@@ -35,12 +33,16 @@ for item in all_data:
     if match:
         full_name = match.group(1).strip()
         correo = match.group(2)
-        if ' ' in full_name:
-            first_name, last_name = full_name.split(' ', 1)
+        # Verificar si "@" no está en full_name
+        if "@" not in full_name:
+            if ' ' in full_name:
+                first_name, last_name = full_name.split(' ', 1)
+            else:
+                first_name = full_name
+                last_name = ""
+            csv_data.append([first_name, last_name, correo])
         else:
-            first_name = full_name
-            last_name = ""
-        csv_data.append([first_name, last_name, correo])
+            csv_data.append(["", "", item])  # Si "@" está en el nombre, colocar el valor completo en "Email"
     else:
         csv_data.append(["", "", item])  # Si no se encuentra el patrón, colocar el valor completo en "Email"
 
