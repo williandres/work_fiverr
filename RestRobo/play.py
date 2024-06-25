e1b8a744f7a7b5566d1c61a2ef493efddc28649c
Willian Cárdenas <sca.willian@gmail.com>
2024-06-24 20:07:05 -0500

    v0.1 testing

--- RestRobo/play.py
+++ RestRobo/play.py
@@ -1,10 +1,17 @@
 import json
 import time
+import inspect
 from pynput.mouse import Button, Controller as MouseController
 from pynput.keyboard import Controller as KeyboardController, Key
 
 time.sleep(5)
-events_file = 'profiles/events421.json'  
+events_file = 'profiles/events262.json'  
+
+key_mapping = {}
+# Iterar sobre los atributos de la clase Key
+for name, obj in inspect.getmembers(Key):
+    if not name.startswith('__') and not callable(obj):
+        key_mapping[f'Key.{name}'] = obj
 
 # Cargar eventos desde el archivo JSON
 def load_events_from_file(filename):
@@ -17,34 +24,41 @@ mouse_controller = MouseController()
 keyboard_controller = KeyboardController()
 
 
+
 def execution_mouse(event):
-    if event['event'] == 'move':
+    # if event['event'] == 'move':
+    #     mouse_controller.position = tuple(event['position'])
+    if event['event'] == 'click' and event['pressed'] == True:
         mouse_controller.position = tuple(event['position'])
-    elif event['event'] == 'click' and event['pressed'] == True:
         time.sleep(1)
         if event['button'] == "Button.left":
             mouse_controller.press(Button.left)
         elif event['button'] == "Button.right":
             mouse_controller.press(Button.right)
-    elif event['event'] == 'click' and event['pressed'] == False:
-        time.sleep(1)
-        if event['button'] == "Button.left":
-            mouse_controller.release(Button.left)
-        elif event['button'] == "Button.right":
-            mouse_controller.release(Button.right)
-    #on scroll - click scroll
+        elif event['button'] == "Button.middle":
+            mouse_controller.press(Button.middle)
+    # elif event['event'] == 'click' and event['pressed'] == False:
+    #     time.sleep(1)
+    #     if event['button'] == "Button.left":
+    #         mouse_controller.release(Button.left)
+    #     elif event['button'] == "Button.right":
+    #         mouse_controller.release(Button.right)
+    #     elif event['button'] == "Button.middle":
+    #         mouse_controller.release(Button.middle)
+    elif event['event'] == 'scroll':
+        mouse_controller.scroll(int(event['dx']), int(event['dy']))
 
 def execution_keyboard(event):
     time.sleep(1)
     key = str(event['key']).replace("'", "")
     if event['event'] == 'press' and key != 'null':
         if '.' in key:
-            key = globals()[key]
-        keyboard_controller.press(Key.space)
+            key = key_mapping[key]
+        keyboard_controller.press(key)
     elif event['event'] == 'release' and key != 'null':
         if '.' in key:
-            key = globals()[key]
-        keyboard_controller.release(Key.space)
+            key = key_mapping[key]
+        keyboard_controller.release(key)
 
 
 
